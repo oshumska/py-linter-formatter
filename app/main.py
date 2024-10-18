7794af20bb48e8b98ad652e186c19a5b56a10a5d
Olha Shumska <olgashumska65@gmail.com>
2024-10-18 15:37:46 +0200

    Code style corrected

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def format_linter_error(error: dict) -> dict:
     return {"line": error["line_number"],
-            "column" : error["column_number"],
+            "column": error["column_number"],
             "message": error["text"],
             "name": error["code"], "source": "flake8"}
 
@@ -12,5 +12,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file , linter_report[file])
+    return [format_single_linter_file(file, linter_report[file])
             for file in linter_report]
